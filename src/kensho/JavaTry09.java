727d8939f63ac9d2903171fd3b60e7492db21790
cohhey <cohhey@hotmail.com>
2024-09-02 00:15:15 +0900

    プロジェクト作成

---
+++ src/kensho/JavaTry09.java
@@ -0,0 +1,11 @@
+package kensho;
+
+import java.util.List;
+
+public class JavaTry09 {
+
+	public static void main(String[] args) {
+		List<String> list = List.of("a","b","c","d");
+		System.out.println(list);
+	}
+}
